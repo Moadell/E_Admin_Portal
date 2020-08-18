10dff6b492440c7c051ff7106f8f3f1656bfd642
Moadell <mo.adel@nu.edu.eg>
2020-08-18 03:26:18 +0200

    Updates

---
+++ precache-manifest.948c60bc75734b34418cd481b5e24984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef46a9829a955a77cddbf5c0d58702fe",
+    "url": "/E_Admin_Portal/index.html"
+  },
+  {
+    "revision": "91f133b6592fed7963dd",
+    "url": "/E_Admin_Portal/static/js/2.90a496aa.chunk.js"
+  },
+  {
+    "revision": "dff9d9c5316d08c3a9a64cbf3df6c258",
+    "url": "/E_Admin_Portal/static/js/2.90a496aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b74104f94677e6de6d",
+    "url": "/E_Admin_Portal/static/js/main.c13f4605.chunk.js"
+  },
+  {
+    "revision": "c7fe040d84197b68253d",
+    "url": "/E_Admin_Portal/static/js/runtime-main.32a6a8a5.js"
+  }
+]);
